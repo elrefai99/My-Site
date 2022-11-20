1742d4474c314abdd495b972cd5e9dde4f9212ab
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-11-20 09:21:33 +0200

    LICENSE

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutViewVue from '@/views/AboutView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutViewVue
   },
 ]
 
---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div class=" flex justify-center items-center w-full h-full">
+            <div class=" m-44">
+                <div class=" p-6 m-10">
+                    ggfgf
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
